1c7531c416709ef9d1b5680b4b0179837c1398ac
Erick Perez <erickjpl@gmail.com>
2025-06-23 09:34:57 -0400

    apuntando al value correcto en el value object number

--- src/plugins/python_django/project_root/src/shared/domain/value_objects/number_value_object.py
+++ src/plugins/python_django/project_root/src/shared/domain/value_objects/number_value_object.py
@@ -3,4 +3,4 @@ from src.shared.domain.value_objects.value_object import ValueObject
 
 class NumberValueObject(ValueObject):
   def is_bigger_than(self, other: "NumberValueObject") -> bool:
-    return self.value > other.value
+    return self._value > other.value
